25dec59c2e5139d52c7fdddaa7ff3028bad0ed2f
佐藤利徳 <tyahhahha@gmail.com>
2018-09-24 20:26:02 +0900

    SEO HTML Head

--- pages/about.vue
+++ pages/about.vue
@@ -1,11 +1,15 @@
 <template>
-  <div>
-    <p>Hi from {{ name }}</p>
+  <div class="container">
+    <h1>About page</h1>
+    <p>Check the source code to see the custom meta tags added with our custom component <code>twitter-head-card</code></p>
+    <twitter-head-card />
     <nuxt-link to="/">Home page</nuxt-link>
   </div>
 </template>
 
 <script>
+import TwitterHeadCard from '~/components/twitter-head-card.vue'
+
 export default {
   asyncData() {
     return {
@@ -13,7 +17,21 @@ export default {
     }
   },
   head: {
-    title: 'About page'
+    title: 'About page',
+    meta: [
+      { hid: 'description', name: 'description', content: 'About page description' }
+    ]
+  },
+  components: {
+    TwitterHeadCard
   }
 }
 </script>
+
+<style>
+.container {
+  text-align: center;
+  margin-top: 150px;
+  font-size: 20px;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="container">
     <h1>Welcome!</h1>
     <nuxt-link to="/about">About page</nuxt-link>
   </div>
@@ -8,7 +8,26 @@
 <script>
 export default {
   head: {
-    title: 'Home page'
-  }
+    title: 'Home page',
+    meta: [
+      { hid: 'description', name: 'description', content: 'Home page description' }
+    ],
+    noscript: [
+      { innerHTML: 'Body No Scripts', body: true }
+    ],
+    script: [
+      { src: '/head.js' },
+      { src: '/body.js', body: true },
+      { src: '/defer.js', defer: '' }
+    ]
+  },
 }
 </script>
+
+<style>
+.container {
+  text-align: center;
+  margin-top: 150px;
+  font-size: 20px;
+}
+</style>
